a2ce778352b1aec659674916587bd6bd4e2cd785
Jarpen94 <kwiatowskijarek@o2.pl>
2018-11-20 14:09:48 +0100

    adding route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import Route1 from './Route1'
 import Route2 from './Route2'
 import Route3 from './Route3'
 
 const App = (props) => (
     <div>
-        <Route1 />
-        <Route2 />
-        <Route3 />
-    </div>
+        <BrowserRouter>
+            <div>
+                <Route1 />
+                <Route2 />
+                <Route3 />
+            </div>
+        </BrowserRouter >
+    </div >
 )
 
 
