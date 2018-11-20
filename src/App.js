dd3d4d5bb18ca9748ed55cc2336159c7a294e379
Jarpen94 <kwiatowskijarek@o2.pl>
2018-11-20 12:42:19 +0100

    buttons added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React from 'react'
 
+import Button from './Button'
+
 const names = ['Ala', 'Ola', 'Ela']
+const namesList = names.map(name => <li key={name}>{name}</li>)
+
 
 const App = (props) => (
-
-
+  <div>
     <div>
-        <div>
-            {names}
-        </div>
-        <ul>
-            {
-                names.map(name => <li key={name}>{name}</li>)
-            }
-
-        </ul>
+      {names}
     </div>
-
-
+    <ul>
+      {namesList}
+    </ul>
+    <Button 
+      label="Kliknij mnie!"
+      alertText = "Kliknąłeś"
+    />
+    <Button 
+      label="Click me!"
+      alertText = 'Clicked'
+    />
+  </div>
 )
 
 export default App
---
+++ src/Button.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = ({ label, alertText }) => (
+  <div>
+    <button
+      onClick = {() => alert (alertText)}
+    >
+    {label}
+    </button>
+  </div>
+)
+
+export default Button
