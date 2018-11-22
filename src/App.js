31045cc4207b240161539b27f8f8cb39cf633630
Jarpen94 <kwiatowskijarek@o2.pl>
2018-11-22 15:18:22 +0100

    finished day 2 with react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router,Link, Route } from 'react-router-dom'
 import Route1 from './Route1'
 import Route2 from './Route2'
 import Route3 from './Route3'
+import DisplayParam from './DisplayParam';
 
-const App = (props) => (
+const App = (props) =>
+  (
     <div>
-        <BrowserRouter>
-            <div>
-                <Route1 />
-                <Route2 />
-                <Route3 />
-            </div>
-        </BrowserRouter >
-    </div >
-)
+      <Router>
+        <div>
+          <div>
+            <ul>
+              <li><Link to='/route-1'>Route1</Link></li>
+              <li><Link to='/route-2'>Route2</Link></li>
+              <li><Link to='/route-3'>Route3</Link></li>
+            </ul>
+          </div>
+          <Route path="/Route-1" exact={true} component={Route1}></Route>
+          <Route path="/route-2" exact={true} component={Route2}></Route>
+          <Route path="/route-3" exact={true} component={Route3}></Route>
+        <Route path="/param/:key" component={DisplayParam} />
 
+        </div>
+      </Router>
+    </div>
+  )
 
 export default App
---
+++ src/DisplayParam.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const DisplayParam = (props) => (
+    <div>
+{props.match.params.key}
+
+    </div>
+)
+
+export default DisplayParam
